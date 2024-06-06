b14c83e756fdb0cf177dba92267fbe8842444335
lfercorrea <84098072+lfercorrea@users.noreply.github.com>
2024-06-06 11:09:12 -0300

    melhoria na pagina inicial;

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layout')
 @section('content')
-    <div class="row container center">
+    <div class="row container center section-margins">
         <div class="col s12">
             <h5>Gestão do PROAP</h5>
         </div>
     </div>
     <div class="row container">
         <a href="{{ route('site.solicitacoes') }}" class="black-text">
-            <div class="col s6 center waves-effect">
+            <div class="col s4 center waves-effect">
                 <i class="material-icons medium black-text">request_page</i>
                 <div class="container">
                     Solicitações
@@ -15,7 +15,7 @@
             </div>
         </a>
         <a href="{{ route('site.solicitantes') }}" class="black-text">
-            <div class="col s6 center waves-effect">
+            <div class="col s4 center waves-effect">
                 <i class="material-icons medium black-text">people</i>
                 <div class="container">
                     Solicitantes
