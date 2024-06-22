e4fd281c491c00f5192b1de1f391f7ded6702315
lfercorrea <84098072+lfercorrea@users.noreply.github.com>
2024-06-22 17:16:52 -0300

    hotfix

--- resources/views/common/aviso_importacao.blade.php
+++ resources/views/common/aviso_importacao.blade.php
@@ -1,6 +1,6 @@
 <p>
     <ol>
-        <li>o Google <del>Shits</del> Sheets delimita os CSVs com vírgula, diferente do padrão do Excel. aqui é necessário ser delimitado por vírgulas;</li>
+        <li>por padrão, o Google Sheets delimita os CSVs com vírgula e o MS Excel com ponto-e-vírgula. Aqui é necessário ser delimitado por vírgulas;</li>
         <li>após a primeira importação, não deve ser alterado nada na planilha compartilhada pelo Gustavo. o sistema espera que novos dados sempre sejam inseridos na ultima linha, e ele grava as informações de cada linha importada anteriormente para poder comparar.</li>
     </ol>
 </p>
