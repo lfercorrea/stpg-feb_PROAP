bb5348cc36ab9fd88d7d5897eef16c74866cfb03
lfercorrea <84098072+lfercorrea@users.noreply.github.com>
2024-06-28 13:46:54 -0300

    removida identação indevida;

--- resources/views/solicitacao.blade.php
+++ resources/views/solicitacao.blade.php
@@ -133,97 +133,97 @@
             @endif
         </div>
         <div class="row">
-                @if ($solicitacao->notas->isNotEmpty())
-                    <div class="section-margins">
-                        <h6>Notas/recibos</h6>
-                    </div>
-                    <table class="compact-table striped responsive-table">
+            @if ($solicitacao->notas->isNotEmpty())
+                <div class="section-margins">
+                    <h6>Notas/recibos</h6>
+                </div>
+                <table class="compact-table striped responsive-table">
+                    <tr>
+                        <th class="center-align print-hidden"><i class="material-icons tiny">delete</i></th>
+                        <th>Nº</th>
+                        <th>Data</th>
+                        <th>Descrição</th>
+                        <th>Fonte pagadora</th>
+                        <th>Tipo de despesa</th>
+                        <th>Valor</th>
+                    </tr>
+                    @foreach ($solicitacao->notas as $nota)
                         <tr>
-                            <th class="center-align print-hidden"><i class="material-icons tiny">delete</i></th>
-                            <th>Nº</th>
-                            <th>Data</th>
-                            <th>Descrição</th>
-                            <th>Fonte pagadora</th>
-                            <th>Tipo de despesa</th>
-                            <th>Valor</th>
+                            <td class="center-align print-hidden"><a href="{{ route('site.nota.destroy', ['solicitacao_id' => $solicitacao->id, 'nota_id' => $nota->id]) }}" class="confirm-link"><i class="material-icons tiny red-text">delete</i></a></td>
+                            <td>{{ $nota->numero }}</td>
+                            <td>{{ \Carbon\Carbon::parse($nota->data)->format('d/m/Y') }}</td>
+                            <td>{{ $nota->descricao }}</td>
+                            <td>{{ $nota->fonte_pagadora->nome }}</td>
+                            <td>{{ $nota->valor_tipo->nome }}</td>
+                            <td>R$&nbsp;{{ number_format($nota->valor, 2, ',', '.') }}</td>
                         </tr>
-                        @foreach ($solicitacao->notas as $nota)
-                            <tr>
-                                <td class="center-align print-hidden"><a href="{{ route('site.nota.destroy', ['solicitacao_id' => $solicitacao->id, 'nota_id' => $nota->id]) }}" class="confirm-link"><i class="material-icons tiny red-text">delete</i></a></td>
-                                <td>{{ $nota->numero }}</td>
-                                <td>{{ \Carbon\Carbon::parse($nota->data)->format('d/m/Y') }}</td>
-                                <td>{{ $nota->descricao }}</td>
-                                <td>{{ $nota->fonte_pagadora->nome }}</td>
-                                <td>{{ $nota->valor_tipo->nome }}</td>
-                                <td>R$&nbsp;{{ number_format($nota->valor, 2, ',', '.') }}</td>
-                            </tr>
-                        @endforeach
-                    </table>
-                    <table class="compact-table striped responsive-table">
-                        <thead>
-                            <tr>
-                                <th colspan="2" class="center-align">Totais</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td class="right-align"><b>Notas:</b></td>
-                                <td class="right-align"><span class="blue-text text-darken-2"><b>{{ $count_notas }}</b></span></td>
-                            </tr>
-                            <tr>
-                                <td class="right-align"><b>Valor:</b></td>
-                                <td class="right-align"><span class="blue-text text-darken-2"><b>R$&nbsp;{{ $valor_total }}</b></span></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                @endif
-                <div class="section-margins print-hidden">
-                    <h6>Lançar nota/recibo</h6>
-                </div>
-                <div class="row print-hidden">
-                    <form class="col s12" action="{{ route('site.nota.store', ['id' => $solicitacao->id]) }}" method="POST">
-                        @csrf
-                        <div class="row">
-                            <div class="input-field col s7 m6">
-                                <input name="numero" id="numero" type="text" maxlength="255" class="validate" required>
-                                <label for="numero">Número</label>
-                            </div>
-                            <div class="input-field col s5 m2">
-                                <input name="data" id="data" type="date" class="validate" required>
-                                <label for="data">Data</label>
-                            </div>
-                            <div class="input-field col s12 m4">
-                                <select name="fonte_pagadora_id" required>
-                                    <option value="" disabled selected>Selecione</option>
-                                    @foreach ($fontes_pagadoras as $fonte_pagadora)
-                                        <option value="{{ $fonte_pagadora->id }}">{{ $fonte_pagadora->nome }}</option>
-                                    @endforeach
-                                </select>
-                                <label>Fonte pagadora</label>
-                            </div>
-                            <div class="input-field col s12 m6">
-                                <input name="descricao" id="descricao" type="text" class="validate">
-                                <label for="descricao">Descrição/observação</label>
-                            </div>
-                            <div class="input-field col s12 m3">
-                                <select name="valor_tipo_id" required>
-                                    <option value="" disabled selected>Selecione</option>
-                                    @foreach ($valor_tipos as $valor_tipo)
-                                        <option value="{{ $valor_tipo->id }}">{{ $valor_tipo->nome }}</option>
-                                    @endforeach
-                                </select>
-                                <label>Tipo de despesa</label>
-                            </div>
-                            <div class="input-field col s8 m2">
-                                <input name="valor" id="valor" type="number" min="0" step="0.01" class="validate" required>
-                                <label for="valor">Valor</label>
-                            </div>
-                            <div class="input-field col s4 m1">
-                                <button class="btn-small green darken-2 waves-effect waves-light" type="submit" name="action">Salvar</button>
-                            </div>
+                    @endforeach
+                </table>
+                <table class="compact-table striped responsive-table">
+                    <thead>
+                        <tr>
+                            <th colspan="2" class="center-align">Totais</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="right-align"><b>Notas:</b></td>
+                            <td class="right-align"><span class="blue-text text-darken-2"><b>{{ $count_notas }}</b></span></td>
+                        </tr>
+                        <tr>
+                            <td class="right-align"><b>Valor:</b></td>
+                            <td class="right-align"><span class="blue-text text-darken-2"><b>R$&nbsp;{{ $valor_total }}</b></span></td>
+                        </tr>
+                    </tbody>
+                </table>
+            @endif
+            <div class="section-margins print-hidden">
+                <h6>Lançar nota/recibo</h6>
+            </div>
+            <div class="row print-hidden">
+                <form class="col s12" action="{{ route('site.nota.store', ['id' => $solicitacao->id]) }}" method="POST">
+                    @csrf
+                    <div class="row">
+                        <div class="input-field col s7 m6">
+                            <input name="numero" id="numero" type="text" maxlength="255" class="validate" required>
+                            <label for="numero">Número</label>
                         </div>
-                    </form>
-                </div>
+                        <div class="input-field col s5 m2">
+                            <input name="data" id="data" type="date" class="validate" required>
+                            <label for="data">Data</label>
+                        </div>
+                        <div class="input-field col s12 m4">
+                            <select name="fonte_pagadora_id" required>
+                                <option value="" disabled selected>Selecione</option>
+                                @foreach ($fontes_pagadoras as $fonte_pagadora)
+                                    <option value="{{ $fonte_pagadora->id }}">{{ $fonte_pagadora->nome }}</option>
+                                @endforeach
+                            </select>
+                            <label>Fonte pagadora</label>
+                        </div>
+                        <div class="input-field col s12 m6">
+                            <input name="descricao" id="descricao" type="text" class="validate">
+                            <label for="descricao">Descrição/observação</label>
+                        </div>
+                        <div class="input-field col s12 m3">
+                            <select name="valor_tipo_id" required>
+                                <option value="" disabled selected>Selecione</option>
+                                @foreach ($valor_tipos as $valor_tipo)
+                                    <option value="{{ $valor_tipo->id }}">{{ $valor_tipo->nome }}</option>
+                                @endforeach
+                            </select>
+                            <label>Tipo de despesa</label>
+                        </div>
+                        <div class="input-field col s8 m2">
+                            <input name="valor" id="valor" type="number" min="0" step="0.01" class="validate" required>
+                            <label for="valor">Valor</label>
+                        </div>
+                        <div class="input-field col s4 m1">
+                            <button class="btn-small green darken-2 waves-effect waves-light" type="submit" name="action">Salvar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
     <div class="container center print-hidden">
